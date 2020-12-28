405d5b444cad71ff3cfd70a07471c17a34f7da9b
Richard van den Berg <richard@vdberg.org>
2020-12-28 14:18:49 +0100

    Initial release

--- README.md
+++ README.md
@@ -1,1 +1,97 @@
-# intergas-exporter
+# This is intergas-exporter
+
+An Intergas boiler data exporter for prometheus
+
+Uses the python evohomeclient from:  
+https://pypi.org/project/evohomeclient/  
+https://github.com/watchforstock/evohome-client
+
+Inspired by the code samples posted on
+https://www.circuitsonline.net/forum/view/80667
+
+## Example output
+
+> $ curl -s http://localhost:8080  
+> &#35; HELP python_gc_objects_collected_total Objects collected during gc  
+> &#35; TYPE python_gc_objects_collected_total counter  
+> python_gc_objects_collected_total{generation="0"} 66.0  
+> python_gc_objects_collected_total{generation="1"} 281.0  
+> python_gc_objects_collected_total{generation="2"} 0.0  
+> &#35; HELP python_gc_objects_uncollectable_total Uncollectable object found during GC  
+> &#35; TYPE python_gc_objects_uncollectable_total counter  
+> python_gc_objects_uncollectable_total{generation="0"} 0.0  
+> python_gc_objects_uncollectable_total{generation="1"} 0.0  
+> python_gc_objects_uncollectable_total{generation="2"} 0.0  
+> &#35; HELP python_gc_collections_total Number of times this generation was collected  
+> &#35; TYPE python_gc_collections_total counter  
+> python_gc_collections_total{generation="0"} 41.0  
+> python_gc_collections_total{generation="1"} 3.0  
+> python_gc_collections_total{generation="2"} 0.0  
+> &#35; HELP python_info Python platform information  
+> &#35; TYPE python_info gauge  
+> python_info{implementation="CPython",major="3",minor="7",patchlevel="3",version="3.7.3"} 1.0  
+> &#35; HELP process_virtual_memory_bytes Virtual memory size in bytes.  
+> &#35; TYPE process_virtual_memory_bytes gauge  
+> process_virtual_memory_bytes 4.2053632e+07  
+> &#35; HELP process_resident_memory_bytes Resident memory size in bytes.  
+> &#35; TYPE process_resident_memory_bytes gauge  
+> process_resident_memory_bytes 1.4688256e+07  
+> &#35; HELP process_start_time_seconds Start time of the process since unix epoch in seconds.  
+> &#35; TYPE process_start_time_seconds gauge  
+> process_start_time_seconds 1.60677395231e+09  
+> &#35; HELP process_cpu_seconds_total Total user and system CPU time spent in seconds.  
+> &#35; TYPE process_cpu_seconds_total counter  
+> process_cpu_seconds_total 7685.64  
+> &#35; HELP process_open_fds Number of open file descriptors.  
+> &#35; TYPE process_open_fds gauge  
+> process_open_fds 6.0  
+> &#35; HELP process_max_fds Maximum number of open file descriptors.  
+> &#35; TYPE process_max_fds gauge  
+> process_max_fds 1024.0  
+> &#35; HELP intergas_temperature_celcius Temperature in celsius  
+> &#35; TYPE intergas_temperature_celcius gauge  
+> intergas_temperature_celcius{type="aanvoer"} 44.08  
+> intergas_temperature_celcius{type="flow"} 43.3  
+> intergas_temperature_celcius{type="retour"} 42.92  
+> intergas_temperature_celcius{type="warmwater"} 32.7  
+> intergas_temperature_celcius{type="set"} 60.0  
+> &#35; HELP intergas_water_pressure_bar Water pressure in bar  
+> &#35; TYPE intergas_water_pressure_bar gauge  
+> intergas_water_pressure_bar 1.71  
+> &#35; HELP intergas_display_code Display code  
+> &#35; TYPE intergas_display_code gauge  
+> intergas_display_code 0.0  
+> &#35; HELP intergas_fanspeed_rpm Fan speed in rpm  
+> &#35; TYPE intergas_fanspeed_rpm gauge  
+> intergas_fanspeed_rpm{type="set"} 0.0  
+> intergas_fanspeed_rpm{type="measured"} 0.0  
+> &#35; HELP intergas_fan_pwm Fan PWM duty cycle percentage  
+> &#35; TYPE intergas_fan_pwm gauge  
+> intergas_fan_pwm 0.0  
+> &#35; HELP intergas_flag Boolean flags  
+> &#35; TYPE intergas_flag gauge  
+> intergas_flag{name="gp_switch"} 1.0  
+> intergas_flag{name="tap_switch"} 0.0  
+> intergas_flag{name="roomtherm"} 0.0  
+> intergas_flag{name="pump"} 1.0  
+> intergas_flag{name="dwk"} 1.0  
+> intergas_flag{name="alarm_status"} 0.0  
+> intergas_flag{name="ch_cascade_relay"} 0.0  
+> intergas_flag{name="opentherm"} 0.0  
+> intergas_flag{name="gasvalve"} 0.0  
+> intergas_flag{name="spark"} 0.0  
+> intergas_flag{name="io_signal"} 1.0  
+> intergas_flag{name="ch_ot_disabled"} 0.0  
+> intergas_flag{name="low_water_pressure"} 0.0  
+> intergas_flag{name="pressure_sensor"} 0.0  
+> intergas_flag{name="burner_block"} 0.0  
+> intergas_flag{name="grad_flag"} 0.0  
+> &#35; HELP intergas_ionization_current_microampere Ionization current in µA  
+> &#35; TYPE intergas_ionization_current_microampere gauge  
+> intergas_ionization_current_microampere 0.0  
+> &#35; HELP intergas_updated Intergas client last updated  
+> &#35; TYPE intergas_updated gauge  
+> intergas_updated 1.609160872862161e+09  
+> &#35; HELP intergas_up Intergas client status  
+> &#35; TYPE intergas_up gauge  
+> intergas_up 1.0  
---
+++ intergas-exporter.py
@@ -0,0 +1,138 @@
+#! /usr/bin/python3
+import sys, serial, time
+import prometheus_client as prom
+
+def Getfloat(lsb, msb):
+  if msb > 127:
+    value = -(float((msb ^ 255) + 1) * 256 - lsb) / 100
+  else:
+    value = float(msb * 256 + lsb) / 100
+  return value
+
+def read_intergas():
+  # Set COM port config
+  ser = serial.Serial()
+  ser.baudrate = 9600
+  ser.timeout=10
+  ser.port="/dev/ttyUSB0"
+
+  # Open COM port
+  try:
+      ser.open()
+  except Exception as e:
+      sys.exit("Fout bij het openen van %s.\n%s: %s".format(ser.name, type(e).__name__, str(e)))
+
+  unixtime_utc = time.time()
+  try:
+      ser.write(b'S?\r')
+  except Exception as e:
+      sys.exit("Seriele poort %s kan niet geschreven worden.\n%s: %s".format(ser.name, type(e).__name__, str(e)))
+
+  try:
+      ig_raw = ser.read(32)
+  except Exception as e:
+      sys.exit("Seriele poort %s kan niet gelezen worden.\n%s: %s".format(ser.name, type(e).__name__, str(e)))
+
+  data = {};
+  if len(ig_raw) == 32:
+    data['timestamp']          = unixtime_utc
+    data['t_aanvoer']          = Getfloat(ig_raw[0] , ig_raw[1])
+    data['t_flow']             = Getfloat(ig_raw[2] , ig_raw[3])
+    data['t_retour']           = Getfloat(ig_raw[4] , ig_raw[5])
+    data['t_warmw']            = Getfloat(ig_raw[6] , ig_raw[7])
+    data['t_boiler']           = Getfloat(ig_raw[8] , ig_raw[9])
+    data['t_buiten']           = Getfloat(ig_raw[10], ig_raw[11])
+    data['ch_pressure']        = Getfloat(ig_raw[12], ig_raw[13])
+    data['temp_set']           = Getfloat(ig_raw[14], ig_raw[15])
+    data['fanspeed_set']       = Getfloat(ig_raw[16], ig_raw[17]) * 100
+    data['fanspeed']           = Getfloat(ig_raw[18], ig_raw[19]) * 100
+    data['fan_pwm']            = Getfloat(ig_raw[20], ig_raw[21]) * 10
+    data['io_curr']            = Getfloat(ig_raw[22], ig_raw[23])
+    data['displ_code']         = ig_raw[24]
+    data['gp_switch']          = bool(ig_raw[26] &  1 << 7)
+    data['tap_switch']         = bool(ig_raw[26] &  1 << 6)
+    data['roomtherm']          = bool(ig_raw[26] &  1 << 5)
+    data['pump']               = bool(ig_raw[26] &  1 << 4)
+    data['dwk']                = bool(ig_raw[26] &  1 << 3)
+    data['alarm_status']       = bool(ig_raw[26] &  1 << 2)
+    data['ch_cascade_relay']   = bool(ig_raw[26] &  1 << 1)
+    data['opentherm']          = bool(ig_raw[26] &  1 << 0)
+    data['gasvalve']           = bool(ig_raw[28] &  1 << 7)
+    data['spark']              = bool(ig_raw[28] &  1 << 6)
+    data['io_signal']          = bool(ig_raw[28] &  1 << 5)
+    data['ch_ot_disabled']     = bool(ig_raw[28] &  1 << 4)
+    data['low_water_pressure'] = bool(ig_raw[28] &  1 << 3)
+    data['pressure_sensor']    = bool(ig_raw[28] &  1 << 2)
+    data['burner_block']       = bool(ig_raw[28] &  1 << 1)
+    data['grad_flag']          = bool(ig_raw[28] &  1 << 0)
+
+    # Change display code if a fault occurred
+    if ig_raw[27] == 128:
+      fault_code = ig_raw[29]
+      data['displ_code'] = fault_code + 256
+
+    # Known display codes (see also https://github.com/rickvanderzwet/IntergasBoilerReader/blob/trunk/intergas_prestige_cw6.py)
+    # 0:   "CV brandt",
+    # 51:  "Warm water",
+    # 102: "CV brandt",
+    # 126: "CV in rust",
+    # 204: "Tapwater nadraaien",
+    # 231: "CV nadraaien",
+
+  # Close port and show status
+  try:
+      ser.close()
+  except Exception as e:
+      sys.exit("Fout blij sluiten van %s.\n%s: %s".format(ser.name, type(e).__name__, str(e)))
+
+  return data
+
+if __name__ == '__main__':
+  temp        = prom.Gauge('intergas_temperature_celcius'           , 'Temperature in celsius', ['type'])
+  pressure    = prom.Gauge('intergas_water_pressure_bar'            , 'Water pressure in bar')
+  dispcode    = prom.Gauge('intergas_display_code'                  , 'Display code')
+  f_speed     = prom.Gauge('intergas_fanspeed_rpm'                  , 'Fan speed in rpm', ['type'])
+  f_pwm       = prom.Gauge('intergas_fan_pwm'                       , 'Fan PWM duty cycle percentage')
+  flag        = prom.Gauge('intergas_flag'                          , 'Boolean flags', ['name'])
+  io_curr     = prom.Gauge('intergas_ionization_current_microampere', 'Ionization current in µA')
+  updated     = prom.Gauge('intergas_updated'                       , 'Intergas client last updated')
+  up          = prom.Gauge('intergas_up'                            , 'Intergas client status')
+  prom.start_http_server(8080)
+
+  while True:
+    ig=read_intergas()
+    if ig:
+      up.set(1)
+      updated.set(ig['timestamp'])
+      temp.labels('aanvoer').set(ig['t_aanvoer'])
+      temp.labels('flow').set(ig['t_flow'])
+      temp.labels('retour').set(ig['t_retour'])
+      temp.labels('warmwater').set(ig['t_warmw'])
+      temp.labels('set').set(ig['temp_set'])
+      pressure.set(ig['ch_pressure'])
+      dispcode.set(ig['displ_code'])
+      f_speed.labels('set').set(ig['fanspeed_set'])
+      f_speed.labels('measured').set(ig['fanspeed'])
+      f_pwm.set(ig['fan_pwm'])
+      io_curr.set(ig['io_curr'])
+
+      flag.labels('gp_switch').set(ig['gp_switch'])
+      flag.labels('tap_switch').set(ig['tap_switch'])
+      flag.labels('roomtherm').set(ig['roomtherm'])
+      flag.labels('pump').set(ig['pump'])
+      flag.labels('dwk').set(ig['dwk'])
+      flag.labels('alarm_status').set(ig['alarm_status'])
+      flag.labels('ch_cascade_relay').set(ig['ch_cascade_relay'])
+      flag.labels('opentherm').set(ig['opentherm'])
+      flag.labels('gasvalve').set(ig['gasvalve'])
+      flag.labels('spark').set(ig['spark'])
+      flag.labels('io_signal').set(ig['io_signal'])
+      flag.labels('ch_ot_disabled').set(ig['ch_ot_disabled'])
+      flag.labels('low_water_pressure').set(ig['low_water_pressure'])
+      flag.labels('pressure_sensor').set(ig['pressure_sensor'])
+      flag.labels('burner_block').set(ig['burner_block'])
+      flag.labels('grad_flag').set(ig['grad_flag'])
+    else:
+      up.set(0)
+
+    time.sleep(5)
